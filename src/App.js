b4e6626d38b8ba52b5b0c678fe29bfa8466dfdbc
Muhaab0 <muuhaab@gmail.com>
2023-01-12 23:46:10 +0200

    a

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
     <BrowserRouter>
       <Routes>
+        <Route path="/egcoco3" element={<Home />}/>
         <Route path="/" element={<Home />}/>
         <Route path="/ourworks" element={<Works />}/>
         <Route path="/about" element={<About />}/>
